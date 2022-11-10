a8c741bf75dc7eb96e0dd8bbd6ad2b9322e8c85b
DreuDavids <dreu.davids1@gmail.com>
2022-11-10 18:38:48 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class Clima extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: LoadingScreen(),
     );
